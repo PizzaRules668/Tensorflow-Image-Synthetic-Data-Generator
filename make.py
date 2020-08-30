7c0fe36785917a3517c875834ab2ddbaa50a970d
PizzaRules668 <46171020+PizzaRules668@users.noreply.github.com>
2020-08-30 14:31:10 -0500

    Uploaded that code

---
+++ make.py
@@ -0,0 +1,145 @@
+import cv2
+import glob
+import os
+import urllib.request
+import random
+import argparse
+import numpy as np
+
+def load():
+    num = 0
+    for file in glob.glob(args.b + "*.jpg"):
+        print(file)
+        img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
+
+        rocket = cv2.imread(args.i, cv2.IMREAD_UNCHANGED)
+        scale = random.randint(20, 50)
+        rocket = resize(rocket, scale)
+
+        pwd = os.popen("cd").read()
+
+        try:
+            pos = (random.randint(0, img.shape[1]), random.randint(0, img.shape[0]))
+            print("At: X:" + str(pos[0]) + " Y:" + str(pos[1]) + " Scale: " + str(scale))
+
+            insert(img, rocket[:, :, 0:3], pos, rocket[:, :, 3] / 255.0)
+            
+            if not os.path.exists(args.b):
+                os.makedirs(args.b)
+
+            imagepath = args.t + file
+
+            write(file, imagepath, pos, img, rocket, scale)
+            cv2.imwrite(imagepath, img)
+        except:
+            print("Failed to put on " + file)
+        
+
+def download(link):
+    file = open(args.l, "r")
+    links = file.readlines()
+    pic_num = 1
+        
+    if not os.path.exists(args.b):
+        os.makedirs(args.b)
+            
+    for i in links:
+        try:
+            print(i)
+            urllib.request.urlretrieve(i, args.b + str(pic_num) + ".jpg")
+            pic_num += 1
+                
+        except Exception as e:
+            print(str(e))
+
+def resize(img, scale):
+    width = int(img.shape[1] * scale / 100)
+    height = int(img.shape[0] * scale / 100)
+    dim = (width, height)
+
+    return cv2.resize(img, dim, interpolation = cv2.INTER_NEAREST)
+
+def insert(img, img_overlay, pos, alpha_mask):
+    x, y = pos
+
+    y1, y2 = max(0, y), min(img.shape[0], y + img_overlay.shape[0])
+    x1, x2 = max(0, x), min(img.shape[1], x + img_overlay.shape[1])
+
+    y1o, y2o = max(0, -y), min(img_overlay.shape[0], img.shape[0] - y)
+    x1o, x2o = max(0, -x), min(img_overlay.shape[1], img.shape[1] - x)
+
+    if y1 >= y2 or x1 >= x2 or y1o >= y2o or x1o >= x2o:
+        return
+
+    channels = img.shape[2]
+
+    alpha = alpha_mask[y1o:y2o, x1o:x2o]
+    alpha_inv = 1.0 - alpha
+
+    for c in range(channels):
+        try:
+            img[y1:y2, x1:x2, c] = (alpha * img_overlay[y1o:y2o, x1o:x2o, c] + alpha_inv * img[y1:y2, x1:x2, c])
+        except Exception as e:
+            print(str(e))
+
+def write(filename, folder, pos, background, front, scale):
+    x, y = pos
+    file = open(filename.rstrip(".jpg")+".xml", "w")
+    file.write('''<annotation>
+    <folder>{}</folder>
+    <filename>{}</filename>
+    <path>{}</path>
+    <source>
+        <database>Unknown</database>
+	</source>
+	<size>
+		<width>{}</width>
+		<height>{}</height>
+		<depth>{}</depth>
+	</size>
+	<segmented>0</segmented>
+	<object>
+		<name>{}</name>
+		<pose>Unspecified</pose>
+		<truncated>0</truncated>
+		<difficult>0</difficult>
+		<bndbox>
+			<xmin>{}</xmin>
+			<ymin>{}</ymin>
+			<xmax>{}</xmax>
+			<ymax>{}</ymax>
+		</bndbox>
+	</object>
+</annotation>'''.format(folder, 
+                        filename, 
+                        args.b, 
+                        str(background.shape[1]), 
+                        str(background.shape[0]), 
+                        str(background.shape[2]), 
+                        args.o, 
+                        str(x), 
+                        str(y), 
+                        str(x + front.shape[1]), 
+                        str(y + front.shape[0])))
+    file.close()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Makes training data for tensorflow')
+    parser.add_argument("-b", type=str, required=True, help="Path for the background images")
+    parser.add_argument("-i", type=str, required=True, help="Image you would like to detect")
+    parser.add_argument("-t", type=str, required=True, help="Where the Images will go")
+    parser.add_argument("-o", type=str, required=True, help="Objects name")
+
+    parser.add_argument("-d", type=bool, help="Download Images")
+    parser.add_argument("-l", type=str, help="Link to download the images")
+
+    args = parser.parse_args()
+
+    if args.d:
+        download(args.l)
+        print("Delete all of the image you dont want to use")
+        print("Also Delete images that are small")
+        input()
+    
+    load()
