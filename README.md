5990746d6d8832140dcb2bb02fc134489e906f78
PizzaRules668 <46171020+PizzaRules668@users.noreply.github.com>
2020-09-11 11:58:44 -0500

    Added random rotation, and Added xml to csv

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Download the make.py script and put it in your working directory.
 <br/>Make a text file with links to all of the images you would like to download.
 <br/>I used [Fatkun Batch Download Image (Chrome Extension)](https://chrome.google.com/webstore/detail/fatkun-batch-download-ima/nnjjahlikiabnchcpehcpkdeckfgnohf) and put all link in a text file
 ## Running
-``` python
-arguments:
+```
+optional arguments:
   -h, --help  show this help message and exit
   -b B        Path for the background images
-  -i I        Image you would like to detect
+  -i I        The dir that has all of the object you would like to detect
   -t T        Training dir
   -r R        Testing dir
   -o O        Objects name
@@ -16,8 +16,10 @@ arguments:
   -d D        Download Images
   -n N        Number of times each background image will be used
   -p P        Percent of images in train dir
+  -c C        Convert xml to csv
+  -f F        Dir you want the csv files to be made Example
 ```
-Example
-``` python
-python make.py -b img/ -i rocket.png -t images/train/ -r images/test/ -o Rocket -l links.txt -d True -n 2
+### Example Command
+``` bash
+python make.py -b img/ -i rocket/ -t images/train/ -r images/test/ -o Rocket -l links.txt -d True -c True -f data/ -n 2
 ```
--- make.py
+++ make.py
@@ -5,63 +5,82 @@ import urllib.request
 import random
 import argparse
 import numpy as np
+import pandas as pd
+import xml.etree.ElementTree as ET
+import imutils
 
-def load(totalimg):
+# Make the Synthetic images
+def edit(totalimg):
     num = 0
+    imagecount = 0
     count = 1
     totalimg = totalimg * args.n
 
     for a in range(0, args.n):
-        for file in glob.glob(args.b + "*.jpg"):
-            print(file)
-            img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
+        for image in glob.glob(args.i + "*.png"):
+            for file in glob.glob(args.b + "*.jpg"):
+                print(file)
+                img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
 
-            rocket = cv2.imread(args.i, cv2.IMREAD_UNCHANGED)
-            scale = random.randint(20, 50)
-            rocket = resize(rocket, scale)
+                scale = random.randint(20, 50)
 
-            pwd = os.popen("cd").read() + "\\"
-            pwd = pwd.replace("\n", "")
+                pwd = os.popen("cd").read() + "\\"
+                pwd = pwd.replace("\n", "")
 
-            try:
-                pos = (random.randint(0, img.shape[1]), random.randint(0, img.shape[0]))
-
-                print("At: X:" + str(pos[0]) + " Y:" + str(pos[1]) + " Scale: " + str(scale))
-
-                insert(img, rocket[:, :, 0:3], pos, rocket[:, :, 3] / 255.0)
+                background = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+                background = resize(background, scale)
                 
-                if not os.path.exists(args.b):
-                    os.makedirs(args.b) 
+                background = cv2.copyMakeBorder(background, 0, 0, int(background.shape[0]/2), int(background.shape[0]/2), cv2.BORDER_CONSTANT)
 
-                if count / totalimg * 100 <= args.p:
-                    imagepath = args.t + str(count) + ".jpg"
-                    xmlpath = args.t + str(count) + ".xml"
-                    folder = args.t
+                try:
 
-                elif count / totalimg * 100 >= args.p:
-                    imagepath = args.r + str(count) + ".jpg"
-                    xmlpath = args.r + str(count) + ".xml"
-                    folder = args.r
+                    pos = (random.randint(0, img.shape[1]), random.randint(0, img.shape[0]))
+                    angle = random.randint(0, 360)
 
-                folder = folder.split("/")
+                    shape = background.shape
 
-                print(str(round((count / totalimg) * 100, 5)) + "%")
+                    matrix = cv2.getRotationMatrix2D((background.shape[1]/2, background.shape[0]/2), angle, 1)
+                    background = cv2.warpAffine(background, matrix, (background.shape[1], background.shape[0]))
 
-                imagepath = imagepath.replace("/", "\\")
-                xmlpath = xmlpath.replace("/", "\\")
+                    print("At: X:" + str(pos[0]) + " Y:" + str(pos[1]) + " Scale: " + str(scale) + " Angle: " + str(angle))
 
-                print(str(imagepath))
+                    insert(img, background[:, :, 0:3], pos, background[:, :, 3] / 255.0)
+                    
+                    if not os.path.exists(args.b):
+                        os.makedirs(args.b) 
 
-                modpos, pos = IfToLarge(pos, rocket, img)
+                    if count / totalimg * 100 <= args.p:
+                        imagepath = args.t + str(count) + ".jpg"
+                        xmlpath = args.t + str(count) + ".xml"
+                        folder = args.t
 
-                write(xmlpath, folder[-2], pos, img, rocket, scale, pwd, count, modpos)
-                cv2.imwrite(imagepath, img)
-            except Exception as e:
-                print("Failed to open " + file)
-                print(e)
-            
-            count += 1
+                    elif count / totalimg * 100 >= args.p:
+                        imagepath = args.r + str(count) + ".jpg"
+                        xmlpath = args.r + str(count) + ".xml"
+                        folder = args.r
+
+                    folder = folder.split("/")
+
+                    print(str(round((count / totalimg) * 100, 5)) + "%")
+
+                    imagepath = imagepath.replace("/", "\\")
+                    xmlpath = xmlpath.replace("/", "\\")
+
+                    print(str(imagepath))
+
+                    modpos, pos = IfToLarge(pos, background, img)
+
+                    write(xmlpath, folder[-2], pos, img, background, scale, pwd, count, modpos)
+                    cv2.imwrite(imagepath, img)
+                except Exception as e:
+                    print("Failed to open " + file)
+                    print(e)
+                
+                count += 1
+
+    imagecount += 1
         
+# Make the xml file
 def write(filename, folder, pos, background, front, scale, pwd, count, modpos):
     x, y = pos
     file = open(filename, "w")
@@ -103,6 +122,8 @@ def write(filename, folder, pos, background, front, scale, pwd, count, modpos):
                         str(modpos[1])))
     file.close()
 
+# Check to see if bounding box goes off the image
+# If it does it makes the bounding box smaller
 def IfToLarge(pos, front, back):
     oldx = pos[0]
     oldy = pos[1]
@@ -118,6 +139,7 @@ def IfToLarge(pos, front, back):
 
     return (x, y), (oldx, oldy)
 
+# Downloads images from text file
 def download(link):
     file = open(args.l, "r")
     links = file.readlines()
@@ -135,13 +157,15 @@ def download(link):
         except Exception as e:
             print(str(e))
 
+# Resizes the image
 def resize(img, scale):
     width = int(img.shape[1] * scale / 100)
     height = int(img.shape[0] * scale / 100)
     dim = (width, height)
 
-    return cv2.resize(img, dim, interpolation = cv2.INTER_NEAREST)
+    return cv2.resize(img, dim, interpolation=cv2.INTER_NEAREST)
 
+# Puts the image you would like to put on the backgroud
 def insert(img, img_overlay, pos, alpha_mask):
     x, y = pos
 
@@ -165,18 +189,44 @@ def insert(img, img_overlay, pos, alpha_mask):
         except Exception as e:
             print(str(e))
 
+# Counts the total amount of images in you background folder
 def countimg():
     lcount = 0
+    icount = 0
     for file in glob.glob(args.b + "*.jpg"):
         lcount += 1
+    
+    for file in glob.glob(args.i + "*.png"):
+        icount += 1
 
-    return lcount
+    return lcount * icount
+
+# Convert the xml file to csv file
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
 
 if __name__ == '__main__':
-
+    # File arguments
     parser = argparse.ArgumentParser(description='Makes training data for tensorflow')
     parser.add_argument("-b", type=str, required=True, help="Path for the background images")
-    parser.add_argument("-i", type=str, required=True, help="Image you would like to detect")
+    parser.add_argument("-i", type=str, required=True, help="The dir that has all of the object you would like to detect")
     parser.add_argument("-t", type=str, required=True, help="Training dir")
     parser.add_argument("-r", type=str, required=True, help="Testing dir")
     parser.add_argument("-o", type=str, required=True, help="Objects name")
@@ -187,14 +237,29 @@ if __name__ == '__main__':
     parser.add_argument("-n", type=int, default=1, help="Number of times each background image will be used")
     parser.add_argument("-p", type=float, default=20, help="Percent of images in train dir")
 
+    parser.add_argument("-c", type=bool, default=False, help="Convert xml to csv")
+    parser.add_argument("-f", type=str, help="Dir you want the csv files to be made")
+
     args = parser.parse_args()
 
+    # If you want to download images
     if args.d:
         download(args.l)
         print("Delete all of the image you dont want to use")
         print("Also Delete images that are small")
-        input()
-    
+        #input()
+
+    # Counts the total amount of files
     totalimg = countimg()
 
-    load(totalimg)
+    # Make the Synthetic images
+    edit(totalimg)
+
+    # If you want to convert xml to csv
+    if args.c:
+        for folder in [args.t, args.r]:
+            filename = folder.split("/")[-2]
+
+            xml_df = xml_to_csv(folder)
+            xml_df.to_csv((args.f + filename + '_labels.csv'), index=None)
+            print('Successfully converted xml to csv.')
